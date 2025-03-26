11e60d14c8955dee7b8cd49345d1bd238bd01177
Abdoulkarim <17a.karim@gmail.com>
2025-03-26 17:22:48 +0000

    Completed basic features of the game

--- RPS-Project/script.js
+++ RPS-Project/script.js
@@ -1,13 +1,21 @@
 const main = document.querySelector("main");
 const optionsContainer = document.querySelector(".options-container");
-const playerScore = document.querySelector(".player-score");
-const computerScore = document.querySelector(".computer-score ");
+const playerScoreEl = document.querySelector(".player-score span");
+const computerScoreEl = document.querySelector(".computer-score span");
 const displayPlayerChoice = document.querySelector(".display-player-choice  ");
 const displayComputerChoice = document.querySelector(
   ".display-computer-choice  "
 );
+const resultEl = document.querySelector(".result");
+resultEl.style.display = "none";
 
+let playerScore = 0;
+let computerScore = 0;
+let result = "";
+let isPlaying = true;
 optionsContainer.addEventListener("click", (e) => {
+  if (!isPlaying) return;
+
   //? Get human choice
   const humanChoice = e.target.closest(".option");
   if (!humanChoice) return;
@@ -17,6 +25,28 @@ optionsContainer.addEventListener("click", (e) => {
 
   //? Get computer choice
   const computerChoice = getComputerChoice();
+
+  //? Display player choice image in the DOM
+  renderComputerChoice(computerChoice);
+
+  //? Play a round and find winner for each round
+  const humanChoiceName = humanChoice.classList[0];
+  playRound(humanChoiceName, computerChoice);
+
+  //? Check is theres a winner
+  if (playerScore >= 5) {
+    result = "You Win!";
+    displayResult(result);
+    isPlaying = false;
+    return;
+  }
+
+  if (computerScore >= 5) {
+    result = "You Lose!";
+    displayResult(result);
+    isPlaying = false;
+    return;
+  }
 });
 
 function getComputerChoice() {
@@ -29,4 +59,34 @@ function renderPlayerChoice(humanChoice) {
   displayPlayerChoice.textContent = "";
   const selectedEmoji = humanChoice.querySelector("img");
   displayPlayerChoice.insertAdjacentHTML("beforeend", selectedEmoji.outerHTML);
+}
+
+function renderComputerChoice(computerChoice) {
+  displayComputerChoice.textContent = "";
+  const img = document.createElement("img");
+  img.src = `./img/${computerChoice}-emoji.png`;
+  displayComputerChoice.append(img);
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return;
+  }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    playerScoreEl.textContent = ++playerScore;
+  } else {
+    computerScoreEl.textContent = ++computerScore;
+  }
+}
+
+function displayResult(result) {
+  resultEl.style.display = "block";
+  const h3 = document.createElement("h3");
+  h3.textContent = result;
+  resultEl.append(h3);
 }
