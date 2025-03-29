b90f335b488ed6c4c2c96f6c1a6e207d55eadac2
Abdoulkarim <17a.karim@gmail.com>
2025-03-29 13:58:37 +0000

    Completed etch a sketch

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -5,9 +5,12 @@ const randomColorEl = document.getElementById("random-color");
 const eraseColorEl = document.getElementById("erase-color");
 const customColorEl = document.getElementById("custom-color");
 const colorPicker = document.getElementById("color-picker");
+const borderColorPickerEl = document.getElementById("border-color");
+const canvasColorPickerEl = document.getElementById("canvas-color");
+const clearBtn = document.querySelector("button");
 
 // An initial grid size of 16x16
-createBoxes(60);
+createBoxes(50);
 
 //* Functionality for the range slider and adding boxes
 range.addEventListener("input", (e) => {
@@ -31,32 +34,56 @@ range.addEventListener("input", (e) => {
   createBoxes(value);
 });
 
+const hoveredBoxes = [];
+//* Functionality for hovering over the canvas
+container.addEventListener("mouseover", (e) => {
+  const randomColor = getRandomColors();
+  const box = e.target;
+  if (box === container) return;
+
+  //? Check which colors radio option is checked
+  if (randomColorEl.checked) box.style.background = randomColor;
+  if (eraseColorEl.checked) box.style.background = "transparent";
+  if (customColorEl.checked) box.style.background = colorPicker.value;
+
+  //? Keep track of hovered boxes.
+  /* 
+  when it comes time for clearing boxes. its more efficient
+  to target the hovered boxes instead of all of them
+  */
+  if (!hoveredBoxes.includes(box)) hoveredBoxes.push(box);
+});
+
+//* Functionality for changing the border colors
+borderColorPickerEl.addEventListener("change", (e) =>
+  updateBorderColor(e.target.value)
+);
+
+//* Functionality for changing the canvas colors
+canvasColorPickerEl.addEventListener("input", (e) =>
+  updateCanvasColor(e.target.value)
+);
+
+//* Functionality for clearing the grid colors
+clearBtn.addEventListener("click", () => clearGrid());
+
 function createBoxes(gridSize) {
   container.textContent = "";
   const totalBoxes = gridSize * gridSize;
+  //? create a temp container to store the boxes
+  //* This is better for performance
+  const tempContainer = document.createDocumentFragment();
 
   for (let i = 0; i < totalBoxes; i++) {
     const box = document.createElement("div");
 
     box.style.width = `calc(100% / ${gridSize})`;
     box.style.height = `calc(100% / ${gridSize})`;
-    container.append(box);
+    tempContainer.append(box);
   }
+  //? Append all at once instead of each interation in the loop
+  container.append(tempContainer);
 }
-
-//* Functionality for hovering over the canvas
-
-container.addEventListener("mouseover", (e) => {
-  const randomColor = getRandomColors();
-  const canvasColor = eraseColors();
-  const box = e.target;
-  if (box === container) return;
-
-  //? Check if Random Colors radio option is checked
-  if (randomColorEl.checked) box.style.background = randomColor;
-  if (eraseColorEl.checked) box.style.background = canvasColor;
-  if (customColorEl.checked) box.style.background = colorPicker.value;
-});
 
 function getRandomColors() {
   const [R, G, B] = Array.from({ length: 3 }, () =>
@@ -71,8 +98,14 @@ The erase functionality works by settings the individual
 boxes inside to match the containers (canvas) color.
 */
 
-function eraseColors() {
-  const containerColor = getComputedStyle(container).backgroundColor;
+function updateBorderColor(color) {
+  document.documentElement.style.setProperty("--border-color", color);
+}
 
-  return containerColor;
+function updateCanvasColor(color) {
+  container.style.background = color;
+}
+
+function clearGrid() {
+  hoveredBoxes.forEach((box) => (box.style.background = "transparent"));
 }
