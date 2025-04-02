8abfa327508781a6ae0ed9bcf2b0bfa302f0f8db
Abdoulkarim <17a.karim@gmail.com>
2025-04-02 13:52:02 +0100

    Completed ui

---
+++ Calculator/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const numbersEl = document.querySelectorAll(".numbers");
+const operatorsEl = document.querySelectorAll(".operator");
+const accessibilitiesEl = document.querySelectorAll(".accessibility");
