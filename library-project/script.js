4527c663e5f80e6217cd848927cfea111b552be2
Abdoulkarim <17a.karim@gmail.com>
2025-04-15 15:24:47 +0100

    Completed basic project layout

---
+++ library-project/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+console.log(crypto.randomUUID());
