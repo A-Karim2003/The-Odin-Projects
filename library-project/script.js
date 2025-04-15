0084cc6299334162d2c53fa523671e6b8e3ef7ad
Abdoulkarim <17a.karim@gmail.com>
2025-04-15 20:03:34 +0100

    added a feature that extracts relevant data from files such as image cover

--- library-project/script.js
+++ library-project/script.js
@@ -5,9 +5,7 @@ const modal = document.querySelector(".modal");
 const inputs = document.querySelectorAll("input");
 const addBookBtn = document.getElementById("add-book-btn");
 const form = document.querySelector("form");
-
-console.log(inputs);
-
+const formInputsEl = document.querySelectorAll(".form-elements");
 /*======================================================*/
 const myLibrary = [];
 //? Add functionality for handling modal
@@ -22,35 +20,58 @@ document.body.addEventListener("click", (e) => {
     modal.classList.remove("active");
   }
 });
+/*======================================================*/
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Submitted");
+
+  const data = Array.from(formInputsEl).reduce((acc, element) => {
+    let value;
+
+    if (element.type === "file") {
+      const file = element.files[0];
+      const file2 = element.files;
+      console.log(file);
+      console.log(file2);
+
+      value = file ? URL.createObjectURL(file) : "";
+    } else {
+      value = element.value.trim();
+    }
+    acc[element.id] = value;
+    return acc;
+  }, {});
+  // console.log(data);
+
+  addBookToLibrary(data[0], data[1], data[2], data[3], data[4]);
 });
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, cover, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.cover = cover;
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, cover, read) {
+  const book = new Book(title, author, pages, cover, read);
   myLibrary.push(book);
+  insertIntoDom(myLibrary);
 }
 
 function insertIntoDom(library) {
+  bookShelf.textContent = "";
   library.forEach((book) => {
-    const { title, author, pages, read } = book;
+    const { title, author, pages, cover, read } = book;
 
     const bookRowHTML = `
       <tr>
           <td>${title}</td>
           <td>${author}</td>
           <td>${pages}</td>
-          <td><img src="" alt="Cover image"></td>
+          <td><img src="${cover}" alt="Cover image"></td>
           <td>
             <button class="toggle-read-btn read">
             ${read ? "Read" : "Not read"}
@@ -58,9 +79,6 @@ function insertIntoDom(library) {
           </td>
       </tr>
     `;
-
     bookShelf.insertAdjacentHTML("beforeend", bookRowHTML);
   });
 }
-
-insertIntoDom(myLibrary);
